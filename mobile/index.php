9bd7099355d8dae247097d68aaef8e47861a5f5d
Carin Hansen <carinhansen95@gmail.com>
2017-03-08 11:48:55 +0100

    Scores

--- mobile/index.php
+++ mobile/index.php
@@ -58,51 +58,51 @@ include 'FootballData.php';
                 // fetch and dump summary data for premier league' season 2015/16
                 $soccerseason = $api->getSoccerseasonById(398);
                 echo "<p><hr><p>"; ?>
-                <h3>Fixtures for the 1st matchday of <?php echo $soccerseason->payload->caption; ?></h3>
-                <table class="table table-striped">
-                    <tr>
-                        <th>HomeTeam</th>
-                        <th></th>
-                        <th>AwayTeam</th>
-                        <th colspan="3">Result</th>
-                    </tr>
-                    <?php foreach ($soccerseason->getFixturesByMatchday(1) as $fixture) { ?>
-                        <tr>
-                            <td><?php echo $fixture->homeTeamName; ?></td>
-                            <td>-</td>
-                            <td><?php echo $fixture->awayTeamName; ?></td>
-                            <td><?php echo $fixture->result->goalsHomeTeam; ?></td>
-                            <td>:</td>
-                            <td><?php echo $fixture->result->goalsAwayTeam; ?></td>
-                        </tr>
-                    <?php } ?>
-                </table>
-                <?php
-                echo "<p><hr><p>";
-                // fetch all available upcoming fixtures for the next week and display
-                $now = new DateTime();
-                $end = new DateTime(); $end->add(new DateInterval('P7D'));
-                $response = $api->getFixturesForDateRange($now->format('Y-m-d'), $end->format('Y-m-d'));
-                ?>
-                <h3>Upcoming fixtures next 7 days</h3>
-                <table class="table table-striped">
-                    <tr>
-                        <th>HomeTeam</th>
-                        <th></th>
-                        <th>AwayTeam</th>
-                        <th colspan="3">Result</th>
-                    </tr>
-                    <?php foreach ($response->fixtures as $fixture) { ?>
-                        <tr>
-                            <td><?php echo $fixture->homeTeamName; ?></td>
-                            <td>-</td>
-                            <td><?php echo $fixture->awayTeamName; ?></td>
-                            <td><?php echo $fixture->result->goalsHomeTeam; ?></td>
-                            <td>:</td>
-                            <td><?php echo $fixture->result->goalsAwayTeam; ?></td>
-                        </tr>
-                    <?php } ?>
-                </table>
+<!--                <h3>Fixtures for the 1st matchday of --><?php //echo $soccerseason->payload->caption; ?><!--</h3>-->
+<!--                <table class="table table-striped">-->
+<!--                    <tr>-->
+<!--                        <th>HomeTeam</th>-->
+<!--                        <th></th>-->
+<!--                        <th>AwayTeam</th>-->
+<!--                        <th colspan="3">Result</th>-->
+<!--                    </tr>-->
+<!--                    --><?php //foreach ($soccerseason->getFixturesByMatchday(1) as $fixture) { ?>
+<!--                        <tr>-->
+<!--                            <td>--><?php //echo $fixture->homeTeamName; ?><!--</td>-->
+<!--                            <td>-</td>-->
+<!--                            <td>--><?php //echo $fixture->awayTeamName; ?><!--</td>-->
+<!--                            <td>--><?php //echo $fixture->result->goalsHomeTeam; ?><!--</td>-->
+<!--                            <td>:</td>-->
+<!--                            <td>--><?php //echo $fixture->result->goalsAwayTeam; ?><!--</td>-->
+<!--                        </tr>-->
+<!--                    --><?php //} ?>
+<!--                </table>-->
+<!--                --><?php
+//                echo "<p><hr><p>";
+//                // fetch all available upcoming fixtures for the next week and display
+//                $now = new DateTime();
+//                $end = new DateTime(); $end->add(new DateInterval('P7D'));
+//                $response = $api->getFixturesForDateRange($now->format('Y-m-d'), $end->format('Y-m-d'));
+//                ?>
+<!--                <h3>Upcoming fixtures next 7 days</h3>-->
+<!--                <table class="table table-striped">-->
+<!--                    <tr>-->
+<!--                        <th>HomeTeam</th>-->
+<!--                        <th></th>-->
+<!--                        <th>AwayTeam</th>-->
+<!--                        <th colspan="3">Result</th>-->
+<!--                    </tr>-->
+<!--                    --><?php //foreach ($response->fixtures as $fixture) { ?>
+<!--                        <tr>-->
+<!--                            <td>--><?php //echo $fixture->homeTeamName; ?><!--</td>-->
+<!--                            <td>-</td>-->
+<!--                            <td>--><?php //echo $fixture->awayTeamName; ?><!--</td>-->
+<!--                            <td>--><?php //echo $fixture->result->goalsHomeTeam; ?><!--</td>-->
+<!--                            <td>:</td>-->
+<!--                            <td>--><?php //echo $fixture->result->goalsAwayTeam; ?><!--</td>-->
+<!--                        </tr>-->
+<!--                    --><?php //} ?>
+<!--                </table>-->
 
                 <?php
                 echo "<p><hr><p>";
@@ -134,27 +134,27 @@ include 'FootballData.php';
 
 
 
-                <?php
-                echo "<p><hr><p>";
-                // fetch players for a specific team
-                $team = $api->getTeamById($searchQuery->teams[0]->id);
-                ?>
-                <h3>Players of <?php echo $team->_payload->name; ?></h3>
-                <table class="table table-striped">
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Jersey Number</th>
-                        <th>Date of birth</th>
-                    </tr>
-                    <?php foreach ($team->getPlayers() as $player) { ?>
-                        <tr>
-                            <td><?php echo $player->name; ?></td>
-                            <td><?php echo $player->position; ?></td>
-                            <td><?php echo $player->jerseyNumber; ?></td>
-                            <td><?php echo $player->dateOfBirth; ?></td>
-                        </tr>
-                    <?php } ?>
+<!--                --><?php
+//                echo "<p><hr><p>";
+//                // fetch players for a specific team
+//                $team = $api->getTeamById($searchQuery->teams[0]->id);
+//                ?>
+<!--                <h3>Players of --><?php //echo $team->_payload->name; ?><!--</h3>-->
+<!--                <table class="table table-striped">-->
+<!--                    <tr>-->
+<!--                        <th>Name</th>-->
+<!--                        <th>Position</th>-->
+<!--                        <th>Jersey Number</th>-->
+<!--                        <th>Date of birth</th>-->
+<!--                    </tr>-->
+<!--                    --><?php //foreach ($team->getPlayers() as $player) { ?>
+<!--                        <tr>-->
+<!--                            <td>--><?php //echo $player->name; ?><!--</td>-->
+<!--                            <td>--><?php //echo $player->position; ?><!--</td>-->
+<!--                            <td>--><?php //echo $player->jerseyNumber; ?><!--</td>-->
+<!--                            <td>--><?php //echo $player->dateOfBirth; ?><!--</td>-->
+<!--                        </tr>-->
+<!--                    --><?php //} ?>
                 </table>
             </div>
         </div>
