ac7d6eb3c3051270c26d8b6fc698b03b4ff348ed
Admin <arnodejong@outlook.com>
2017-03-15 12:22:13 +0100

    Scores

--- pleinscherm/index.php
+++ pleinscherm/index.php
@@ -22,7 +22,7 @@ include 'FootballData.php';
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8">
-                <iframe width="100%" height="600px" src="https://www.youtube.com/embed/KuqYhSAMqH0" frameborder="0" allowfullscreen></iframe>
+                <iframe width="100%" height="600px" src="https://www.youtube.com/embed/Hs6vJ8W3H4g" frameborder="0" allowfullscreen></iframe>
                 <div class="col-md-12">
                     Top 3
                 </div>
