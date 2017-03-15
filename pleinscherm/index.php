c2f44b9661e6f719a6e2dea8a2a255cecd6e4ce5
Carin Hansen <carinhansen95@gmail.com>
2017-03-15 11:05:24 +0100

    .

--- pleinscherm/index.php
+++ pleinscherm/index.php
@@ -22,11 +22,19 @@ include 'FootballData.php';
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8">
-                <iframe width="100%" height="700px" src="https://www.youtube.com/embed/KuqYhSAMqH0" frameborder="0" allowfullscreen></iframe>
+                <iframe width="100%" height="600px" src="https://www.youtube.com/embed/KuqYhSAMqH0" frameborder="0" allowfullscreen></iframe>
+                <div class=""
+                <div class="col-md-4">
+                    liedje 1
+                </div>
+                <div class="col-md-4">
+                    liedje 2
+                </div>
+                <div class="col-md-4">
+                    liedje 3
+                </div>
             </div>
                 <div class="col-md-4">
-
-
                 <?php
                 // Create instance of API class
                 $api = new FootballData();
