038eb86fc3e5dc5de72ee76163be4ffad7e73110
Carin Hansen <carinhansen95@gmail.com>
2017-03-29 16:40:55 +0200

    sum votes

---
+++ SoundCloud/compare.php
@@ -0,0 +1,29 @@
+<?php
+include 'dbh.php';
+session_start();
+
+// insert scores in db
+$sql = "select SUM(vote) AS total1 from positive_vibe_votes WHERE single='1'";
+$result1 = mysqli_query($conn, $sql);
+$row1 = mysqli_fetch_assoc($result1);
+
+echo $row1['total1'];
+
+// insert scores in db
+$sql1 = "select SUM(vote) AS total2 from positive_vibe_votes WHERE single='2'";
+$result2 = mysqli_query($conn, $sql1);
+$row2 = mysqli_fetch_assoc($result2);
+
+echo $row2['total2'];
+
+
+// insert scores in db
+$sql2 = "select SUM(vote) AS total3 from positive_vibe_votes WHERE single='3'";
+$result3 = mysqli_query($conn, $sql2);
+$row3 = mysqli_fetch_assoc($result3);
+
+echo $row3['total3'];
+
+echo (max($row1['total1'], $row2['total2'], $row3['total3']));
+
+?>
