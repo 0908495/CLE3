ef3802eff214346d662f3177dc98b7f4d61f4d51
Carin Hansen <carinhansen95@gmail.com>
2017-04-03 16:06:48 +0200

    tracks added

--- soundcloud/streaming.php
+++ soundcloud/streaming.php
@@ -1,3 +1,81 @@
+<?php
+include 'dbh.php';
+session_start();
+
+$vibe = $_SESSION['vibe'];
+
+if($vibe == 'goed') {
+// insert scores in db
+    $sql = "select SUM(vote) AS total1 from positive_vibe_votes WHERE single='1'";
+    $result1 = mysqli_query($conn, $sql);
+    $row1 = mysqli_fetch_assoc($result1);
+
+
+// insert scores in db
+    $sql1 = "select SUM(vote) AS total2 from positive_vibe_votes WHERE single='2'";
+    $result2 = mysqli_query($conn, $sql1);
+    $row2 = mysqli_fetch_assoc($result2);
+
+
+// insert scores in db
+    $sql2 = "select SUM(vote) AS total3 from positive_vibe_votes WHERE single='3'";
+    $result3 = mysqli_query($conn, $sql2);
+    $row3 = mysqli_fetch_assoc($result3);
+
+
+    if ($row1['total1'] == (max($row1['total1'], $row2['total2'], $row3['total3']))) {
+        echo "Liedje 1 wint";
+        $track = 123193944;
+        $_SESSION['track'] = $track;
+    } elseif ($row2['total2'] == (max($row1['total1'], $row2['total2'], $row3['total3']))) {
+        echo 'Liedje 2 wint';
+        $track = 211547947;
+        $_SESSION['track'] = $track;
+    } else {
+        echo 'Liedje 3 wint';
+        $track = 192101815;
+        $_SESSION['track'] = $track;
+    }
+} else {
+
+    // insert scores in db
+    $sql = "select SUM(vote) AS total1 from negative_vibe_votes WHERE single='1'";
+    $result1 = mysqli_query($conn, $sql);
+    $row1 = mysqli_fetch_assoc($result1);
+
+
+    // insert scores in db
+    $sql1 = "select SUM(vote) AS total2 from negative_vibe_votes WHERE single='2'";
+    $result2 = mysqli_query($conn, $sql1);
+    $row2 = mysqli_fetch_assoc($result2);
+
+
+    // insert scores in db
+    $sql2 = "select SUM(vote) AS total3 from negative_vibe_votes WHERE single='3'";
+    $result3 = mysqli_query($conn, $sql2);
+    $row3 = mysqli_fetch_assoc($result3);
+
+
+    if ($row1['total1'] == (max($row1['total1'], $row2['total2'], $row3['total3']))) {
+        echo "Liedje 1 wint";
+        $track = 98849365;
+        $_SESSION['track'] = $track;
+    } elseif ($row2['total2'] == (max($row1['total1'], $row2['total2'], $row3['total3']))) {
+        echo 'Liedje 2 wint';
+        $track = 16266948;
+        $_SESSION['track'] = $track;
+    } else {
+        echo 'Liedje 3 wint';
+        // hand in hand
+        $track = 59500241;
+        $_SESSION['track'] = $track;
+    }
+
+}
+
+$track = $_SESSION['track'];
+?>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -33,11 +111,6 @@
     </style>
 </head>
 <body>
-<?php
-session_start();
-$track = 20230709;
-?>
-
 <script type="text/javascript">
     SC.initialize({
         client_id: 'g0ATeGIhNpgzYpEKeegATafCvns2N2Gc'
@@ -52,11 +125,6 @@ $track = 20230709;
     }).catch(function(){
         console.log(arguments);
     });
-
-
-//    player.on('finish', function() {
-//        console.log('hoi');
-//    });
 
 </script>
 </body>
