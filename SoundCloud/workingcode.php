d54db6a65e88bcb2aac5932cb0974db87a6348c6
Admin <arnodejong@outlook.com>
2017-04-05 10:01:09 +0200

    Fixed looping with music

---
+++ SoundCloud/workingcode.php
@@ -0,0 +1,23 @@
+<script type="text/javascript">
+    function nextSong(){
+        SC.initialize({
+            client_id: 'g0ATeGIhNpgzYpEKeegATafCvns2N2Gc'
+        });
+        //'/tracks/' + track.id
+
+        SC.stream('/tracks/' + <?= $track ?>).then(function(player){
+            player.play();
+            player.on('finish', function() {
+                reqwest({
+                    url: 'streaming.php/',
+                    success: nextSong
+                });
+            });
+        }).catch(function(){
+            console.log(arguments);
+        });
+    }
+
+    nextSong();
+
+</script>
